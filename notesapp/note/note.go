6c743a5e6462a80de762885abd7cb5add5e5b38a
Amar3339 <amarg3339@gmail.com>
2026-09-15 16:04:50 +0530

    storing data in json file and converts name of file by the name of title .

---
+++ notesapp/note/note.go
@@ -0,0 +1,39 @@
+package note
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+	"os"
+)
+
+type Note struct {
+Title string
+Content string 
+CreatedAt time.Time
+ }
+func (note Note) Display(){
+	fmt.Printf("Your note titled %v has the following content:\n\n%v",note.Title,note.Content)
+}
+func New(title,content string)(Note,error){
+	if title =="" || content==""{
+		return Note{}, errors.New("please enter valid input")
+
+	}
+	return Note{
+		Title:title,
+		Content: content,
+		CreatedAt: time.Now(), 
+	},nil
+}
+func (note Note) Save()error{
+	fileName:= strings.ReplaceAll(note.Title," ","_")
+	fileName=strings.ToLower(fileName)+".json"
+	json,err:=json.Marshal(note)
+	if err!=nil{
+		return err
+	}
+	return os.WriteFile(fileName,json,0644)
+}
